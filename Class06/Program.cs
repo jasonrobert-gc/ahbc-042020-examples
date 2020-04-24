cff95334e59bdd08724ca3c991a90ed871f96140
jrob5756 <jasont.robert@gmail.com>
2020-04-23 20:40:33 -0400

    class 6

---
+++ Class06/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Class06
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter a number pleeeeeease: ");
+                    string input = Console.ReadLine();
+                    if (input.Equals("N", StringComparison.OrdinalIgnoreCase))
+                    {
+                        break;
+                    }
+
+                    var number = int.Parse(input);
+                    DisplayNumber(number);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine($"Format Boom! {ex}");
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    Console.WriteLine($"The argument was out of range! {ex}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Boom! {ex}");
+                }
+            }
+        }
+
+        public static void DisplayNumber(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number));
+            }
+
+            Console.WriteLine($"Here is your number - {number}");
+        }
+    }
+}
