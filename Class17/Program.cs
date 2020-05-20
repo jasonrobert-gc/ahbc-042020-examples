a43cf1090ec5a9f78c55e08d7aebbf040e3aabf4
Jason Robert <jason@grandcircus.co>
2020-05-20 18:44:13 -0400

    Class 17

---
+++ Class17/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Class17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm just creating this to show how merging works!");
+        }
+    }
+}
