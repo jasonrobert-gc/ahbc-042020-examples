0819ed617e8036f91678bedb53c019ca6dce10fc
Jason Robert <jason@grandcircus.co>
2020-06-18 21:01:41 -0400

    Class 30

---
+++ Bonus1/GradeCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bonus1
+{
+    public class GradeCalculator
+    {
+        public string GetGrade(int grade)
+        {
+            return ""; // "A", "B"...
+        }
+    }
+}
