13f84f6e314ad665c3582b7bfdae876e2a88ca8e
Jason Robert <jason@grandcircus.co>
2020-07-09 20:44:38 -0400

    Class 39

---
+++ Class39/Class39/Controllers/FoodController.cs
@@ -0,0 +1,24 @@
+﻿using Class39.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Class39.Controllers
+{
+    public class FoodController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Food food)
+        {
+            // Save to a database??? 
+            // Typically you would do something with this...
+            // But for now we will just display the results
+
+            return View(food);
+        }
+    }
+}
---
+++ Class39/Class39/Models/Food.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class39.Models
+{
+    public class Food
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+    }
+}
