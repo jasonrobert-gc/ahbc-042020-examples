b81c1a120be0ed4023cd2e72e4d6bf357a334ca8
Jason Robert <jason@grandcircus.co>
2020-07-20 21:13:45 -0400

    Class43

---
+++ Class43/Class43/Controllers/ShoppingCartController.cs
@@ -0,0 +1,46 @@
+﻿using Class43.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Class43.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly IHttpContextAccessor _accessor;
+
+        public ShoppingCartController(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+        public IActionResult Index()
+        {
+            var current = _accessor.HttpContext.Session.GetInt32("quantity");
+            if (current == null)
+            {
+                current = 0;
+            }
+
+            var model = new ShoppingCart
+            {
+                Quantity = current.Value
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Submit()
+        {
+            var current = _accessor.HttpContext.Session.GetInt32("quantity");
+            if (current == null)
+            {
+                current = 0;
+            }
+
+            var newValue = current.Value + 1;
+            _accessor.HttpContext.Session.SetInt32("quantity", newValue);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Class43/Class43/Models/ShoppingCart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class43.Models
+{
+    public class ShoppingCart
+    {
+        public int Quantity { get; set; }
+    }
+}
