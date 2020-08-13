24dab9258c34c60d582365106b06c50e1757a510
Jason Robert <jason@grandcircus.co>
2020-08-12 20:21:27 -0400

    Class 53

---
+++ Class53/Class53/Model/Starship.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Class53.Model
+{
+    public class Starship
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+
+        [JsonPropertyName("cost_in_credits")]
+        public string CostInCredits { get; set; }
+        public string Length { get; set; }
+        public string Max_atmosphering_speed { get; set; }
+        public string Crew { get; set; }
+        public string Passengers { get; set; }
+        public string Cargo_capacity { get; set; }
+        public string Consumables { get; set; }
+        public string Hyperdrive_rating { get; set; }
+        public string MGLT { get; set; }
+        public string starship_class { get; set; }
+        public object[] Pilots { get; set; }
+        public string[] Films { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Edited { get; set; }
+        public string Url { get; set; }
+    }
+
+}
---
+++ Class53/Class53/Program.cs
@@ -0,0 +1,43 @@
+﻿using Class53.Model;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Class53
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://swapi.dev/api/");
+            //var options = new JsonSerializerOptions
+            //{
+            //    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            //};
+
+            //using (var response = await client.GetAsync("starships/9/"))
+            //{
+            //    response.EnsureSuccessStatusCode();
+
+            //    //// BAD WAY - since we pull the entire content into memory 
+            //    //var result = await response.Content.ReadAsStringAsync();
+            //    //var starship = await JsonSerializer.DeserializeAsync<Starship>(result, options);
+
+            //    // Better way - since we use a stream
+            //    using (var stream = await response.Content.ReadAsStreamAsync())
+            //    {
+            //        var ship = await JsonSerializer.DeserializeAsync<Starship>(stream, options);
+            //        //Console.WriteLine(ship.Name);
+            //    }
+            //}
+
+            // Using System.Net.Http.Json!
+            var starship = await client.GetFromJsonAsync<Starship>("starships/9/");
+            Console.WriteLine(starship.Name);
+        }
+    }
+}
